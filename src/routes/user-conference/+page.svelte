6ac3f7603eccd81b2686953fb53ba49aebcf50f8
Rafa Mondal <raf13461@esri.com>
2023-08-24 19:12:03 -0600

    added dummy text

--- src/routes/user-conference/+page.svelte
+++ src/routes/user-conference/+page.svelte
@@ -30,7 +30,7 @@
         </div>
     </header>
     <div class="body">
-        <div class="sm-text">User Conference is the largest GIS Conference in the world. This year, I was able to attend three virtual sessions. </div>
+        <div class="sm-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
     </div>
     <footer>thank you for reading :)<br><br>contact: rafa.audrija.mondal@gmail.com</footer>
 </div>
