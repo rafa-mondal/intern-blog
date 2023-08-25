ebc663571d0a98c6345cf3c174a6c73696e2a701
Rafa Mondal <raf13461@esri.com>
2023-08-25 14:18:49 -0600

    uc and pothole

--- src/routes/pothole/+page.svelte
+++ src/routes/pothole/+page.svelte
@@ -26,7 +26,9 @@
         </div>
     </header>
     <div class="body">
-       <div class="sm-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </div>
+       <div class="sm-text">Of all the projects during my internship, this gave me the most exposure to ArcGIS Pro.</div>
+        <img class="img" src="/images/pothole-view.png" alt=""> 
+        <img class="img" src="/images/pothole-pro.png" alt=""> 
     </div>
     <footer>thank you for reading :)<br><br>contact: rafa.audrija.mondal@gmail.com</footer>
 </div>
